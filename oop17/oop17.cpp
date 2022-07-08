6f898c19fc8a4240a361b6736eaf460135a54096
Zokerzes <romzes85@mail.ru>
2022-07-08 20:08:17 +0300

    Добавьте файлы проекта.

---
+++ oop17/oop17.cpp
@@ -0,0 +1,33 @@
+﻿
+#include<vector>
+#include <iostream>
+using namespace std;
+void print(vector<int>v) {
+	for (int item : v) {
+		cout << item<<endl;
+
+	}
+	cout << endl;
+}
+int main()
+{
+	vector<int> v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+		cout <<"capacity " << v.capacity()<<" size"<<v.size()<<endl;
+	};
+	
+	//print(v);
+	v.begin();
+	vector<int>::iterator iBegin = v.begin();
+	//v.erase(iBegin + 2, iBegin + 6);
+	print(v);
+	//cout<<v.capacity();
+	v.resize(3, 0);
+	print(v);
+	v.resize(30, 0);
+	print(v);
+	
+}
+
